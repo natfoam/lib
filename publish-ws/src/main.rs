a2b64a8084ea1ab5dafce6747a9808f7418578e1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2021-05-14 00:34:25 -0700

    publish-ws

--- publish-ws/src/main.rs
+++ publish-ws/src/main.rs
@@ -1,6 +1,8 @@
+use std::io::{stdout,stderr,Write};
 use toml::from_str;
-use std::fs::read_to_string;
+use std::fs::{read_to_string, canonicalize};
 use serde_derive::Deserialize;
+use std::process::Command;
 
 #[derive(Deserialize, Debug)]
 struct Workspace {
@@ -16,6 +18,11 @@ fn main() {
     let cargo_toml_str = read_to_string("Cargo.toml").unwrap();
     let cargo_toml: CargoToml = from_str(&cargo_toml_str).unwrap();
     for member in cargo_toml.workspace.members {
-        println!("{}", member);    
+        println!("{}", member);
+        let p = canonicalize(member).unwrap();
+        println!("{:?}", p);
+        let output = Command::new("cargo").arg("publish").current_dir(p).output().unwrap();
+        stdout().write_all(&output.stdout).unwrap();
+        stderr().write_all(&output.stderr).unwrap();
     }
 }
