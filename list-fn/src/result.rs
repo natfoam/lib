3e62bac82bc96dd5cb347d77bd58f2f247605a50
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2021-05-15 20:57:26 -0700

    Id

--- list-fn/src/result.rs
+++ list-fn/src/result.rs
@@ -7,3 +7,10 @@ impl ResultFn for () {
     type Result = ();
     fn result(self) {}
 }
+
+struct Id<T> (T);
+
+impl<T> ResultFn for Id<T> {
+    type Result = T;
+    fn result(self) -> T { self.0 }
+}
