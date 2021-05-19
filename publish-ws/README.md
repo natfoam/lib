270261f7b9c482d38cb99f75e41de21077dae411
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2021-05-18 21:22:43 -0700

    publish-ws checks.

--- publish-ws/README.md
+++ publish-ws/README.md
@@ -2,6 +2,11 @@
 
 Publish all workspace packages.
 
-## Limitation
+Compare to other tools
+
+- [cargo-publish-all](https://gitlab.com/torkleyy/cargo-publish-all). It doesn't publish a `README.md` file.
+- [cargo-workspaces](https://github.com/pksunkara/cargo-workspaces). It does require one version for all packages.
+
+## Limitations
 
 Only for Windows.
--- publish-ws/src/main.rs
+++ publish-ws/src/main.rs
@@ -1,9 +1,10 @@
+use serde::{
+    de::{MapAccess, Visitor},
+    Deserialize, Deserializer,
+};
 use serde_derive::Deserialize;
-use std::collections::HashMap;
-use std::fs::read_to_string;
-use std::process::Command;
+use std::{collections::HashMap, fmt, fmt::Formatter, fs::read_to_string, process::Command};
 use toml::from_str;
-use toml::Value;
 
 #[derive(Deserialize, Debug)]
 struct Workspace {
@@ -15,17 +16,59 @@ struct WsCargoToml {
     workspace: Workspace,
 }
 
-type Dependencies = HashMap<String, Value>;
+#[derive(Debug)]
+struct Dependency(String);
+
+impl<'de> Deserialize<'de> for Dependency {
+    fn deserialize<D>(d: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct DependencyVisitor;
+
+        impl<'de> Visitor<'de> for DependencyVisitor {
+            type Value = Dependency;
+            fn expecting(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                f.write_str("String or Table")
+            }
+            fn visit_str<E>(self, value: &str) -> Result<Dependency, E> {
+                Ok(Dependency(value.to_string()))
+            }
+            fn visit_map<V>(self, mut visitor: V) -> Result<Dependency, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut result = None;
+                while let Some((key, value)) = visitor.next_entry::<String, String>()? {
+                    if key == "version" {
+                        result = Some(value);
+                    }
+                }
+                Ok(Dependency(result.unwrap()))
+            }
+        }
+
+        d.deserialize_any(DependencyVisitor)
+    }
+}
+
+type Dependencies = HashMap<String, Dependency>;
+
+#[derive(Deserialize, Debug)]
+struct Package {
+    version: String,
+}
 
 #[derive(Deserialize, Debug)]
 struct CargoToml {
+    package: Package,
     dependencies: Dependencies,
 }
 
 fn main() {
     let ws_cargo_toml_str = read_to_string("Cargo.toml").unwrap();
     let ws_cargo_toml: WsCargoToml = from_str(&ws_cargo_toml_str).unwrap();
-    let mut map: HashMap<String, Dependencies> = ws_cargo_toml
+    let mut map: HashMap<String, CargoToml> = ws_cargo_toml
         .workspace
         .members
         .iter()
@@ -33,22 +76,55 @@ fn main() {
             let cargo_toml_file = String::from(".\\") + &member + "\\Cargo.toml";
             let cargo_toml_str = read_to_string(cargo_toml_file).unwrap();
             let cargo_toml: CargoToml = from_str(&cargo_toml_str).unwrap();
-            (member.clone(), cargo_toml.dependencies)
+            (member.clone(), cargo_toml)
         })
         .collect();
+    // check versions of internal dependencies
+    for (name, cargo_toml) in map.iter() {
+        for (d_name, d_version) in cargo_toml.dependencies.iter() {
+            if let Some(p) = map.get(d_name) {
+                if p.package.version != d_version.0 {
+                    eprintln!(
+                        "Version mismatch: {}.{} = {}, {} = {}",
+                        name, d_name, d_version.0, d_name, p.package.version
+                    );
+                    return;
+                }
+            }
+        }
+    }
+    for (name, _) in map.iter() {
+        let x = Command::new("cargo")
+            .arg("search")
+            .arg(name)
+            .arg("--limit")
+            .arg("1")
+            .output()
+            .unwrap();
+        let line = String::from_utf8(x.stdout)
+            .unwrap()
+            .lines()
+            .nth(0)
+            .unwrap()
+            .to_string();
+        println!("{}", line);
+        let x: HashMap<String, String> = from_str(&line).unwrap();
+        let i = x.iter().nth(0).unwrap();
+        println!("{} = {}", i.0, i.1);
+    }
     // Publish dependent packages first.
     while !map.is_empty() {
         let member = map
             .iter()
-            .find(|(_, dependencies)| {
-                dependencies
+            .find(|(_, cargo_toml)| {
+                cargo_toml
+                    .dependencies
                     .iter()
                     .all(|(dependency, _)| !map.contains_key(dependency))
             })
-            .unwrap()
-            .0
-            .clone();
-        println!("{}", member);
+            .unwrap();
+        println!("{} = {}", member.0, member.1.package.version);
+        let key = member.0.clone();
         println!();
         {
             // TODO:
@@ -59,7 +135,7 @@ fn main() {
             // don't use `canonicalize`. `canonicalize` returns a UNC path.
             // https://github.com/rust-lang/rust/issues/42869
             // `cargo` can't handle properly UNC paths.
-            let p = String::from(".\\") + &member;
+            let p = String::from(".\\") + &key;
             let _ = Command::new("cargo")
                 .arg("publish")
                 .current_dir(p)
@@ -68,6 +144,6 @@ fn main() {
                 .wait();
         }
         println!();
-        map.remove(&member);
+        map.remove(&key);
     }
 }
