4eae73774ebce06d4651a542e9f32f3fd0505559
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-07-05 10:58:39 -0700

    utilities

--- xpcom/src/main.rs
+++ xpcom/src/main.rs
@@ -1,10 +1,10 @@
+mod guid;
+
+use guid::Guid;
+use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use serde::{Serialize, Deserialize, de::Visitor};
-use serde_derive::{Deserialize, Serialize};
-use serde::de::Error;
-
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 enum Type {
     I8,
     U8,
@@ -18,13 +18,13 @@ enum Type {
     Id(String),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Param {
     name: String,
     r#type: Type,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Method {
     name: String,
     params: Vec<Param>,
@@ -33,72 +33,58 @@ struct Method {
 
 type Struct = Vec<Param>;
 
-struct Guid (u128);
-
-impl Serialize for Guid {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer {
-        serializer.serialize_str(format!("{:X}", self.0).as_str())
-    }
-}
-
-struct StringVisitor;
-
-impl<'de> Visitor<'de> for StringVisitor {
-    type Value = String;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        formatter.write_str("a string GUID")
-    }
-
-    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
-        where
-            E: Error, {
-        Ok(v)
-    }
-}
-
-impl<'de> Deserialize<'de> for Guid {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de> {
-        let s = deserializer.deserialize_string(StringVisitor)?;
-        let x = u128::from_str_radix(s.as_str(), 16);
-        match x {
-            Ok(v) => Ok(Guid(v)),
-            Err(e) => Err(e).map_err(D::Error::custom)
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize)]
 enum TypeDef {
     Struct(Struct),
     Interface { guid: Guid, methods: Vec<Method> },
 }
 
+fn type_def(name: &str, def: TypeDef) -> (String, TypeDef) {
+    (name.to_string(), def)
+}
+
+fn struct_(name: &str, params: &[Param]) -> (String, TypeDef) {
+    type_def(name, TypeDef::Struct(params.to_vec()))
+}
+
+fn interface(name: &str, guid: u128, methods: &[Method]) -> (String, TypeDef) {
+    type_def(
+        name,
+        TypeDef::Interface {
+            guid: Guid(guid),
+            methods: methods.to_vec(),
+        },
+    )
+}
+
+fn method(name: &str, params: &[Param], result: Type) -> Method {
+    Method {
+        name: name.to_string(),
+        params: params.to_vec(),
+        result,
+    }
+}
+
+fn ptr(t: Type) -> Type {
+    Type::Ptr(Box::new(t))
+}
+
+fn param(name: &str, t: Type) -> Param {
+    Param { name: name.to_string(), r#type: t }
+}
+
 type Library = HashMap<String, TypeDef>;
 
 fn main() {
     let library = Library::from([
-        (
-            "S".to_string(),
-            TypeDef::Struct(vec![Param {
-                name: "begin".to_string(),
-                r#type: Type::U8,
-            }]),
+        struct_(
+            "S",
+            &[param("begin",Type::U8)],
         ),
-        (
-            "IMy".to_string(),
-            TypeDef::Interface {
-                guid: Guid(0xFFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF),
-                methods: vec![Method {
-                    name: "GetValue".to_string(),
-                    params: vec![],
-                    result: Type::Ptr(Box::new(Type::U32)),
-                }],
-            },
+        interface(
+            "IMy",
+            0xFFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF,
+            &[method("GetValue", &[], ptr(Type::U32))],
         ),
     ]);
 
