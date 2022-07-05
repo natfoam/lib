81ac52482020a016fe7340e96cf4240c93b01751
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-07-05 10:16:51 -0700

    Type

--- xpcom/src/main.rs
+++ xpcom/src/main.rs
@@ -1,6 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::format};
 
+use serde::{Serialize, Deserialize, de::Visitor};
 use serde_derive::{Deserialize, Serialize};
+use serde::de::Error;
 
 #[derive(Serialize, Deserialize)]
 enum Type {
@@ -31,23 +33,66 @@ struct Method {
 
 type Struct = Vec<Param>;
 
+struct Guid (u128);
+
+impl Serialize for Guid {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer {
+        serializer.serialize_str(format!("{:X}", self.0).as_str())
+    }
+}
+
+struct StringVisitor;
+
+impl<'de> Visitor<'de> for StringVisitor {
+    type Value = String;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a string GUID")
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+        where
+            E: Error, {
+        Ok(v)
+    }
+}
+
+impl<'de> Deserialize<'de> for Guid {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de> {
+        let s = deserializer.deserialize_string(StringVisitor)?;
+        let x = u128::from_str_radix(s.as_str(), 16);
+        match x {
+            Ok(v) => Ok(Guid(v)),
+            Err(e) => Err(e).map_err(D::Error::custom)
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 enum TypeDef {
     Struct(Struct),
-    Interface { guid: String, methods: Vec<Method> },
+    Interface { guid: Guid, methods: Vec<Method> },
 }
 
 type Library = HashMap<String, TypeDef>;
 
 fn main() {
     let library = Library::from([
-        ("S".to_string(), TypeDef::Struct(vec![
-            Param { name: "begin".to_string(), r#type: Type::U8 },
-        ])),
+        (
+            "S".to_string(),
+            TypeDef::Struct(vec![Param {
+                name: "begin".to_string(),
+                r#type: Type::U8,
+            }]),
+        ),
         (
             "IMy".to_string(),
             TypeDef::Interface {
-                guid: "".to_string(),
+                guid: Guid(0xFFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF),
                 methods: vec![Method {
                     name: "GetValue".to_string(),
                     params: vec![],
