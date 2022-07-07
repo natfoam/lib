c6c6b45508e5024aba0bdb3d5bebb7c61f0266d6
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-07-06 22:15:48 -0700

    Interface

--- xpcom/src/lib.rs
+++ xpcom/src/lib.rs
@@ -1,46 +1,55 @@
-use std::sync::atomic::AtomicU32;
-
 type HRESULT = u32;
 
 type GUID = u128;
 
 type ULONG = u32;
 
-struct IUnknown(&'static IUknownVmt);
+//
 
-struct IUknownVmt {
-    QueryInterface: extern "stdcall" fn (this: &mut IUnknown, riid: &GUID, ppvObject: &mut &mut IUnknown) -> HRESULT,
-    AddRef: extern "stdcall" fn (this: &mut IUnknown) -> ULONG,
-    Release: extern "stdcall" fn (this: &mut IUnknown) -> ULONG,
+pub trait Interface: 'static {
+    const ID: GUID;
 }
 
 #[repr(transparent)]
-struct Ref(*mut IUnknown);
+pub struct Obj<I: Interface> (&'static Vmt<I>);
 
-impl Ref {
-    fn raw(&self) -> &mut IUnknown { unsafe { &mut *self.0 } }
-    fn new(raw: &mut IUnknown) -> Self {
+#[repr(C)]
+struct Vmt<I: Interface> {
+    QueryInterface: extern "stdcall" fn (this: &mut Obj<I>, riid: &GUID, ppvObject: &mut &mut Obj<I>) -> HRESULT,
+    AddRef: extern "stdcall" fn (this: &mut Obj<I>) -> ULONG,
+    Release: extern "stdcall" fn (this: &mut Obj<I>) -> ULONG,
+    interface: I
+}
+
+#[repr(transparent)]
+pub struct Ref<I: Interface>(*mut Obj<I>);
+
+impl<I: Interface> Ref<I> {
+    pub fn raw(&self) -> &mut Obj<I> { unsafe { &mut *self.0 } }
+    pub fn new(raw: &mut Obj<I>) -> Self {
         (raw.0.AddRef)(raw);
         Self(raw)
     }
 }
 
-impl Drop for Ref {
+impl<I: Interface> Drop for Ref<I> {
     fn drop(&mut self) {
         let raw = self.raw();
         (raw.0.Release)(raw);
     }
 }
 
-impl Clone for Ref {
+impl<I: Interface> Clone for Ref<I> {
     fn clone(&self) -> Self { Self::new(self.raw()) }
 }
 
+//
+
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+
     }
 }
