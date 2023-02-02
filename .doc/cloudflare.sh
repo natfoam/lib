31e3e5baa0126a9eadab2d7cdce3a5f551f4fd57
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-02-02 11:31:58 -0800

    cloudflare doc deployment (#13)
    * cloudflare
    
    * /settings
    
    * redirect to bin-tree
    
    * cp
    
    * cloudflare
    
    * point to bin_tree
    
    * bin_tree
    
    * netlify and cloudflare
    
    * netlify

--- .doc/cloudflare.sh
+++ .doc/cloudflare.sh
@@ -1,2 +1,3 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 ~/.cargo/bin/cargo doc
+cp ./.doc/index.html ./target/doc/
---
+++ .doc/netlify.sh
@@ -0,0 +1,3 @@
+# rustup update default stable
+cargo doc
+cp ./.doc/index.html ./target/doc/
